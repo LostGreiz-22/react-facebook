7bd740499bc0bb4dcfceeaacfbb416b090a55277
LostGreiz-22 <lnnnkojp@gmail.com>
2022-09-10 14:01:50 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import './App.css';
 
 function App() {
-  return (
+  return ( <div className ="App">
+    <Home />
+  </div>
     
    
   );
---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from "../components/Header";
+
+export default function Home() {
+  return (
+    <div className="h-screen bg-gray-50 overflow-hidden">
+        {/* Header */}
+        <Header />
+
+
+
+        {/* Sidebar */}
+
+        {/*Feed */}
+
+        {/*Widgets */}
+    </div>
+  )
+}
