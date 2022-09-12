d32697fb44fd9c8b036ad3c2bf238bc3a89736f2
LostGreiz-22 <lnnnkojp@gmail.com>
2022-09-12 19:03:48 +0800

    updated codes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from "react";
+import Feed from "../components/Feed";
 import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
 export default function Home() {
   return (
-    <div className="h-screen bg-gray-50 overflow-hidden">
-        {/* Header */}
-        <Header />
+    <div className="h-screen bg-gray-100 overflow-hidden">
+      {/* Header */}
+      <Header />
 
-
-
+      <main className="flex">
         {/* Sidebar */}
+        <Sidebar />
 
-        {/*Feed */}
+        {/* Feed */}
+        <Feed />
 
-        {/*Widgets */}
+        {/* Widgets */}
+        <Widgets />
+      </main>
     </div>
-  )
+  );
 }
